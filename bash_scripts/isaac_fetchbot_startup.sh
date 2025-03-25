88bd77a87bef5c195c0e433984a548a997ab4e6c
bd <70774.2143@compuserve.com>
2025-03-25 13:12:01 -0400

    initial commit

---
+++ bash_scripts/isaac_fetchbot_startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# THis is: isaac_fetchbot_startup.sh
+# THis runs on R16 to launch things needed for Fetchbot
+# To start from comand line (eventually start from a systemd service upon startup
+#   bd@R16:~$ /home/bd/workspaces_nondocker/isaac_ros-dev/src/isaac_fetchbot/bash_scripts/isaac_fetchbot_startup.sh
+
+# RVIZ2
+gnome-terminal --title="RVIZ2" --geometry=49x11+0+0  -- $SHELL -c "cd ~/workspaces_nondocker/isaac_ros-dev && rviz2"
+
+# NAV2 BRINGUP MAP SERVER & AMCL
+gnome-terminal --title="MAP SERVER & AMCL" --geometry=49x11+100+50  -- $SHELL -c "cd ~/workspaces_nondocker/isaac_ros-dev && ros2 launch nav2_bringup r16_localization_launch.py map:=/home/bd/workspaces_nondocker/isaac_ros-dev/src/isaac_fetchbot/maps/my_map_1618_combined_windows_restored.yaml"
+
+# SET INITIAL POSITION
+gnome-terminal --title="SET INITIAL POSITION" --geometry=49x11+200+100  -- $SHELL -c "ros2 topic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped '{ header: {stamp: {sec: 0, nanosec: 0}, frame_id: "map"}, pose: { pose: {position: {x: 0.0, y: 0.0, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}, } }'"
+
+# TELEOP TWIST
+gnome-terminal --title="TELEOP TWIST" --geometry=49x11+0+0  -- $SHELL -c "ros2 run teleop_twist_keyboard teleop_twist_keyboard"
