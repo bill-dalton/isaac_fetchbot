e9c86cece7be21b0de1773bfc0d782739ed335b0
bd <70774.2143@compuserve.com>
2025-09-07 16:28:32 -0400

    fixed incorrect map scaling, problem was scaling of rplidar output on /scan. added 'my_scan_scaler.cpp to fix

--- bash_scripts/isaac_fetchbot_startup.sh
+++ bash_scripts/isaac_fetchbot_startup.sh
@@ -7,6 +7,8 @@
 #   bd@R16:~$ ros2 run tf2_tools view_frames
 #   bd@R16:~$ ros2 run tf2_ros tf2_echo map tag16h5:8
 #   bd@R16:~$ ros2 run tf2_ros tf2_monitor map   tag16h5:8
+#   bd@R16:~$ ros2 topic pub --once /voice_commands std_msgs/msg/String '{data: "Felicity pivot 90 degrees"}'
+#   bd@R16:~$ ros2 topic pub --once /voice_commands std_msgs/msg/String '{data: "Felicity go to the table"}'
 
 # CHRONY - This just cchecks, otherwise doesn't do anything
 # gnome-terminal --title="CHRONY" --geometry=60x12 -- $SHELL -c "chronyc -n tracking"
@@ -27,6 +29,13 @@ gnome-terminal --title="MAP SERVER" --geometry=49x11+100+100  -- $SHELL -c "cd ~
 
 # NAV2 BRINGUP KEEPOUT SERVER
 gnome-terminal --title="KEEPOUT SERVER" --geometry=49x11+200+200  -- $SHELL -c "cd ~/workspaces_nondocker/isaac_ros-dev && ros2 launch nav2_bringup r16_nav2_keepout_server_launch.py"
+
+# KEEPOUT & /cmd_vel RELAYS
+gnome-terminal --title="KEEPOUT RELAY LOCAL" --geometry=49x11+450+450  -- $SHELL -c "cd ~/workspaces_nondocker/isaac_ros-dev && ros2 run topic_tools relay /keepout_filter_mask /local_costmap/keepout_filter_mask"
+gnome-terminal --title="KEEPOUT RELAY GLOBAL" --geometry=49x11+500+500  -- $SHELL -c "cd ~/workspaces_nondocker/isaac_ros-dev && ros2 run topic_tools relay /keepout_filter_mask /global_costmap/keepout_filter_mask"
+gnome-terminal --title="/cmd_vel RELAY" --geometry=49x11+500+500  -- $SHELL -c "cd ~/workspaces_nondocker/isaac_ros-dev && ros2 run topic_tools relay /cmd_vel_nav /cmd_vel"
+
+sleep 5
 
 # NAV2 BRINGUP R16_NAVIGATION_LAUNCH - BRINGS UP... moved back to Orin 6/17/25
   # Brings up: controller_server', 'smoother_server', 'planner_server', 'behavior_server', 'bt_navigator', 'waypoint_follower', 'velocity_smoother'
@@ -60,16 +69,20 @@ gnome-terminal --title="SET INITIAL POSITION" --geometry=49x11+250+250  -- $SHEL
 # VUI OLLAMA BRIDGE
 gnome-terminal --title="VUI OLLAMA BRIDGE" --geometry=100x30+800+400  -- $SHELL -c "ros2 run vui_ollama_bridge vui_ollama_bridge_node"
 
+# START OLLAMA-MISTRAL MODEL
+gnome-terminal --title="OLLAMA RUN MISTRAL-NEMO" --geometry=49X11+850+450  -- $SHELL -c "ollama run mistral-nemo"
+# bd@R16:~$ ollama run mistral-nemo
+
 # TOPIC ECHOS
-#gnome-terminal --title="/behavior_tree_log"  --geometry=60x30+0+200  -- $SHELL -c "ros2 topic echo /behavior_tree_log"
-#gnome-terminal --title="/voice_commands"  --geometry=60x30+0+200  -- $SHELL -c "ros2 topic echo /voice_commands"
-#gnome-terminal --title="/fb_tasks"        --geometry=60x30+50+225  -- $SHELL -c "ros2 topic echo /fb_tasks"
-#gnome-terminal --title="/fb_speaks"       --geometry=60x30+100+250  -- $SHELL -c "ros2 topic echo /fb_speaks"
+#gnome-terminal --title="/behavior_tree_log"  --geometry=60x20+0+200  -- $SHELL -c "ros2 topic echo /behavior_tree_log"
+gnome-terminal --title="/voice_commands"  --geometry=60x20+0+200  -- $SHELL -c "ros2 topic echo /voice_commands"
+#gnome-terminal --title="/fb_tasks"        --geometry=60x20+50+225  -- $SHELL -c "ros2 topic echo /fb_tasks"
+gnome-terminal --title="/fb_speaks"       --geometry=60x20+100+250  -- $SHELL -c "ros2 topic echo /fb_speaks"
 #gnome-terminal --title="/fb_requests"     --geometry=60x30+150+275  -- $SHELL -c "ros2 topic echo /fb_requests"
-#gnome-terminal --title="/fb_responses"    --geometry=60x30+200+300  -- $SHELL -c "ros2 topic echo /fb_responses"
-#gnome-terminal --title="/bearing_heading_turn"    --geometry=60x30+200+325  -- $SHELL -c "ros2 topic echo /bearing_heading_turn"
-#gnome-terminal --title="/heading"         --geometry=60x30+200+450  -- $SHELL -c "ros2 topic echo /heading"
-#gnome-terminal --title="/heading_mag_icm20948"         --geometry=60x30+200+350  -- $SHELL -c "ros2 topic echo /heading_mag_icm20948"
+#gnome-terminal --title="/fb_responses"    --geometry=60x25+200+300  -- $SHELL -c "ros2 topic echo /fb_responses"
+#gnome-terminal --title="/bearing_heading_turn"    --geometry=60x20+200+325  -- $SHELL -c "ros2 topic echo /bearing_heading_turn"
+#gnome-terminal --title="/heading"         --geometry=60x20+200+450  -- $SHELL -c "ros2 topic echo /heading"
+#gnome-terminal --title="/heading_mag_icm20948"         --geometry=60x20+200+350  -- $SHELL -c "ros2 topic echo /heading_mag_icm20948"
 
 # LOWER FINGERS
 gnome-terminal --title="LOWER FINGERS"    --geometry=60x30+200+375  -- $SHELL -c "ros2 topic pub --once /fb_requests geometry_msgs/msg/Vector3 '{x: 14.0, y: 0, z: 0}'"
